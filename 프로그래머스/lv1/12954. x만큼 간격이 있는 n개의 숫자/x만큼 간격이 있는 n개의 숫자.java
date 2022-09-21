327390ecf640f1ab1f257cc8ff084e24c9c1a34f
jeonghui <59547003+kimjeonghui@users.noreply.github.com>
2022-09-21 22:39:29 +0900

    Update x만큼 간격이 있는 n개의 숫자.java
    주석

--- 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
+++ 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
@@ -1,7 +1,7 @@
 class Solution {
     public long[] solution(int x, int n) {
         long[] answer = new long[n];
-        long num = 0;
+        long num = 0; //변수의 범위 확인하고 메모리 할당
         //x를 더한 숫자를 더하는 걸 n만큼 반복
         for(int i=0; i<n; i++){
             num += x;
