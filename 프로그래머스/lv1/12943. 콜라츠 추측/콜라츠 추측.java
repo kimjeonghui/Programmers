69f423462b3ffa53725d6f3148681f76ff5ee055
jeonghui <59547003+kimjeonghui@users.noreply.github.com>
2022-09-23 22:06:23 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        
+        long n = num;
+        if( num ==1) return 0;
+        for(int i=0; i<500; i++){
+            if(n != 1){ //n이 1이 아닌 경우
+                if(n %2 == 0){//짝수인 경우
+                    n /= 2;
+                }
+                else{//홀수인 경우
+                    n = n*3 +1;
+                }
+            }
+            else{//n이 1인 경우
+                return i;
+            }
+            
+        }
+        answer = -1;
+        return answer;
+    }
+}
