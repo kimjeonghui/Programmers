954b4c235f7d8f443845aad7990da562b03c7895
jeonghui <59547003+kimjeonghui@users.noreply.github.com>
2023-02-12 13:09:43 +0900

    [level 1] Title: 폰켓몬, Time: 3.17 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        //검색과 데이터 삽입이 활발해야 함 
+        HashMap<Integer, Integer> hash = new HashMap<Integer, Integer>();
+        for(int i =0; i<nums.length; i++){
+            //해시맵에 포함하고 있지 않을 때
+            if(!hash.containsKey(nums[i])){
+                hash.put(nums[i],1);
+            }
+            else{
+                hash.replace(nums[i], hash.get(nums[i] + 1));
+            }
+        }
+        /* 
+        1. hashmap의 길이가 n/2이상일 경우 
+        2. 미만인 경우는 hashmap 길이 리턴
+        */
+        if(hash.size()>= nums.length/2){
+            answer = nums.length/2;
+        }
+        else{
+           answer =  hash.size();
+        }
+        
+        return answer;
+    }
+}
