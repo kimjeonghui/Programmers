8b9a8b51befedbb61a4ed5cf1f7e2939c2c3f1a6
jeonghui <59547003+kimjeonghui@users.noreply.github.com>
2023-02-14 15:41:45 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 88.73 ms, Memory: 95.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,17 +1,33 @@
 import java.util.*;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        //ArrayList<String> comple= new ArrayList<String>(Arrays.asList(completion));
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        
-        for(int i=0; i<participant.length; i++){
-            if(!(participant[i].equals(completion[i]))){
-                answer+= participant[i];
-                break;
+        //동명이인 해결 불가
+        // String answer = "";
+        // List<String> p = Arrays.asList(completion);
+        // for(String folks: participant){
+        //     if(!p.contains(folks)){
+        //         answer = folks;
+        //     }
+        // }
+        // return answer;
+        String answer ="";
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String p: participant){
+            if(map.containsKey(p)){
+                map.replace(p, map.get(p)+1);
+            }
+            else{
+                map.put(p,1);
             }
         }
+        for(String c: completion){
+            map.replace(c, map.get(c)-1);
+        }
+        for(String s : map.keySet()){
+            if(map.get(s) != 0) answer= s;
+        }
+        
         return answer;
     }
 }
