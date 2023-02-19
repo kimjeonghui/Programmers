bd92bd3c09a3d3df762fe13b6438208805f4fa80
jeonghui <59547003+kimjeonghui@users.noreply.github.com>
2023-02-19 17:25:30 +0900

    [level 2] Title: 프린터, Time: 0.48 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프린터/프린터.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 1;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0; i<priorities.length; i++){
+            pq.add(priorities[i]);
+        }
+        while(!pq.isEmpty()){
+            for(int i=0; i<priorities.length; i++){
+                if(priorities[i] == pq.peek()){
+                    if(i == location){
+                        return answer;
+                    }
+                    else {
+                        pq.poll();
+                        answer++;
+                    }
+                }
+                
+                
+            }
+        }
+        return answer;
+    }
+}
