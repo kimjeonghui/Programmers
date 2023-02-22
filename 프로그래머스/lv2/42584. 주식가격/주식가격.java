f4fe0595d1d45207a5667fb0afc1144a3d3a53a7
jeonghui <59547003+kimjeonghui@users.noreply.github.com>
2023-02-22 21:50:22 +0900

    [level 2] Title: 주식가격, Time: 77.23 ms, Memory: 68.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -1,34 +1,46 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
-        //큐
-        //이중반복문
-        //뒤에서 두번째는 무조건 1
-        //뒤에서 마지막은 무조건 0
-        List<Integer> result = new ArrayList<>();
-        for(int i=0; i<prices.length; i++){
-            if( i == prices.length-1){
-                result.add(0);
-                break;
-            }
-            if( i == prices.length-2){
-                result.add(1);
-                continue;
-            }
-            int cnt =0;
-            for(int j =i+1; j<prices.length; j++){
-                if(prices[i] <= prices[j]) cnt++;
-                else{
-                    cnt++;
+        Deque<Integer> queue = new ArrayDeque<>();
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < prices.length; i++) {
+            //if(i != prices.length-1)queue.add(0);
+            for (int j = i ; j < prices.length-1; j++) {
+                if (i == prices.length - 1) {
                     break;
                 }
+                if (prices[i] > prices[j]) {
+                    break;
+                } else {
+                    queue.add(0);
+                }
+
+
             }
-            result.add(cnt);
+                list.add(queue.size());
+                queue.clear();
         }
-        int[] answer = result.stream()
-            .mapToInt( i -> i)
-            .toArray();
-        
-        return answer;
+        System.out.println(list.stream().mapToInt(i -> i).toArray());
+        return list.stream().mapToInt(i -> i).toArray();
+//         Stack<Integer> beginIdxs = new Stack<>();
+//         int i=0;
+//         int[] terms = new int[prices.length];
+
+//         beginIdxs.push(i);
+//         for (i=1; i<prices.length; i++) {
+//             while (!beginIdxs.empty() && prices[i] < prices[beginIdxs.peek()]) {
+//                 int beginIdx = beginIdxs.pop();
+//                 terms[beginIdx] = i - beginIdx;
+//             }
+//             beginIdxs.push(i);
+//         }
+//         while (!beginIdxs.empty()) {
+//             int beginIdx = beginIdxs.pop();
+//             terms[beginIdx] = i - beginIdx - 1;
+//         }
+
+//         return terms;
     }
 }
+      
