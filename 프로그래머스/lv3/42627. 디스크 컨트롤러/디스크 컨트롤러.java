aabd6b3f464810f110a51dd3cf1c2ccac789df57
jeonghui <59547003+kimjeonghui@users.noreply.github.com>
2023-03-02 19:09:23 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.26 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -13,22 +13,21 @@ class Solution {
     }
 });
         PriorityQueue<int[]> readyQ =  new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-        //Queue<int[]> playQ =  new LinkedList<>();
+
         for(int i=0; i<jobs.length; i++){
             q.add(jobs[i]);
         }
-        int[] now = q.poll();
-        answer+= now[1];
-        end += (now[1] + now[0]);
-        
+
+        int[] now = new int[2];
         while(q.size() >0){
             if(readyQ.isEmpty()){
-                
-                if (q.peek()[0] > end && readyQ.isEmpty()){
+                //마지막 작업시간보다 늦게 시작되는 경우
+                if (q.peek()[0] > end ){
                     now = q.poll();
                     answer+= now[1];
                     end = (now[1] + now[0]);
                     }
+                //마지막 작업 시간 전에 시작되는 경우
                 while(q.peek()!= null ){
                     if(q.peek()[0] <= end){
                         readyQ.add(q.poll());
@@ -37,27 +36,19 @@ class Solution {
                 }
                 
             }
-    
+                //대기하고 있던 작업들 처리
                 while(!readyQ.isEmpty()){
-                    
                     now = readyQ.poll();
-                    //바로 실행되지 않는 경우
-                    if(end < now[0]){
-                        answer+= now[1];
-                    }
-                    else{
-                        answer += (end - now[0] + now[1]);
-                    }
+                    answer += (end - now[0] + now[1]);
                     end += now[1];
+                    //현 작업이 진행되고 있던 사이 대기하는 작업들 있나 확인
                     while(q.peek()!= null ){
                     if(q.peek()[0]<= end){
                         readyQ.add(q.poll());
                     }
                     else break;
-                }
-                
+                }  
             }
-            //실행된 동안 레디큐에 추가
         }
         
         
