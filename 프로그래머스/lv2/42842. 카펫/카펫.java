894c9c548d52953d55822997ec30ef2989e7911c
jeonghui <59547003+kimjeonghui@users.noreply.github.com>
2023-03-15 23:17:42 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        //x 가로, y 세로
+        //x * y = brown + yellow
+        //(x-2)(y-2) =xy -2x -2y+4 =  yellow
+        //(x+y)**2 = x**2 + y**2 + 2xy = (브라운+옐로우)**2
+        //2(x +y) -4 = 브라운
+        int sum =brown + yellow;
+        int XPlusY = (brown+4)/2;
+        int XMulY = brown + yellow;
+        
+        int x =1;
+        boolean check =true;
+        //int y =1;
+        //가로는 세로와 같거나 더 큼
+       while(check){
+           
+           for(int i=3; i<=x; i++){
+               if(sum % i ==0){
+                if(x+ i == XPlusY && x*i == XMulY)  {
+                    answer[0] = x;
+                    answer[1] = i;
+                    check = false;
+                    break;
+                }
+               }
+               //x++;
+           }
+           x++;
+       }
+        return answer;
+    }
+}
