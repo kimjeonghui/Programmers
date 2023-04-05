c1a6e67499ea4f398a63a19d915fad2c32c0cc6a
jeonghui <59547003+kimjeonghui@users.noreply.github.com>
2023-04-05 11:09:38 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.06 ms, Memory: 87.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String solution(String number, int k) {
+        //String answer = "";
+        int index = 0;
+        int range =0;
+        
+        //StringBuilder사용 안할 시 시간 초과
+        StringBuilder sb = new StringBuilder();
+        
+        for(int m=0; m<number.length()-k; m++){
+             int max =0;
+           
+                // i<k+m :테스트 통과 못함
+            // max = Integer.parseInt(String.valueOf(number.charAt(i)) : 시간초과
+                for(int i =index; i<=k+m; i++){
+              
+                if( max < number.charAt(i) -'0'){
+                   max = number.charAt(i) -'0'; 
+                    index = i+1;
+                    if(max ==9 )break;
+                    }  
+                }
+                sb.append( String.valueOf(max));
+        
+        }
+           
+
+        
+        return sb.toString();
+    }
+}
