85b8c87ed380cf57a798641a8a61fa407617aaab
jeonghui <59547003+kimjeonghui@users.noreply.github.com>
2023-08-24 20:08:41 +0900

    [level 3] Title: 네트워크, Time: 0.30 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.java
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -1,27 +1,47 @@
+import java.util.*;
 class Solution {
-    static boolean[] visited;
-  
+    static int answer =0;
+    
     public int solution(int n, int[][] computers) {
-        //dfs로 연결된지 확인 -> 스택
-        //visited[]
-        visited = new boolean[n];
-        int cnt= 0;
-        for(int i=0; i<n; i++){
-            if(!visited[i]){
-               dfs(i, computers);
-            cnt++; 
+        boolean[] visited = new boolean[computers.length];
+        Stack<Integer> stack = new Stack<Integer>();
+        //dfs
+       
+        for(int i=0; i<computers.length;i++){
+            if(visited[i] != true){
+                stack.add(i); 
+            dfs(stack,computers, visited);
+             }
+        }
+           
+        
+        
+        
+            
+        return answer;
+    }
+    
+    public static void dfs( Stack<Integer> stack , int[][] computers, boolean[] visited){
+        while(!stack.isEmpty()){
+            int i =stack.pop();
+            if(visited[i]!= true){
+                
+                visited[i] = true;
+            for(int j=0; j< computers.length; j++){
+                if(i != j && visited[j]!=true &&computers[i][j] == 1){
+                    stack.push(j);
+                }
+            }
+            
+                
             }
             
         }
-   
-        return cnt;
-    }
-    public static void dfs(int i, int[][] computers){
-        visited[i] = true;
-        for(int j =0; j<computers[i].length; j++){
-            if( i != j &&visited[j] != true && computers[i][j] ==1 ){
-                dfs(j,computers);
-            }
-        }
-    }
+        answer++;
+        
+                }
+            
+        
+    
+    
 }
