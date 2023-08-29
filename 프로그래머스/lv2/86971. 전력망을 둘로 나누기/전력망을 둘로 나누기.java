52134e087be6d1743bcd129bceb6d4484ece8495
jeonghui <59547003+kimjeonghui@users.noreply.github.com>
2023-08-29 21:03:07 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 10.01 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,56 +1,57 @@
 import java.util.*;
-
 class Solution {
+    int answer = 99999;
     public int solution(int n, int[][] wires) {
-     /*
-        2차원배열로 연결 표시
-        for문으로 wires 반복해서 첫번째부터 연결을 끊음
-        answer = Math. min(answer, | 전체 노드의 수 - 연결이 끊길 때 노드의 수| )
-        */
-        int index = n;
-        int[][] matrix = new int[index][index];
-
-        //연결된 노드 표시
-        for(int i=0; i<wires.length; i++){
-            int a = wires[i][0]-1;
-            int b = wires[i][1]-1;
-            matrix[a][b] = 1;
-            matrix[b][a] = 1;
+        boolean[] visited = new boolean[n+1];
+        int[][] matrix = new int[n+1][n+1];
+        //초기화
+        for(int i =0; i< wires.length; i++){
+            matrix[wires[i][0]][wires[i][1]]= 1;
+            matrix[wires[i][1]][wires[i][0]]= 1;
         }
-
-        int answer = Integer.MAX_VALUE;
-
+        
         for(int i =0; i<wires.length; i++){
-            int a = wires[i][0]-1;
-            int b = wires[i][1]-1;
-            matrix[a][b] = 0;
-            matrix[b][a] = 0;
-
-            boolean[] visited = new boolean[index];
-            int nodeCount = 0;
-
-            Stack<Integer> stack = new Stack<>();
-            stack.push(0);
-            visited[0] = true;
-
-            while(!stack.isEmpty()){
-                int node = stack.pop();
-                nodeCount++;
-
-                for(int j=0; j<index; j++){
-                    if(matrix[node][j] == 1 && !visited[j]){
-                        stack.push(j);
-                        visited[j] = true;
+            matrix[wires[i][0]][wires[i][1]]= 0;
+            matrix[wires[i][1]][wires[i][0]]= 0;
+            
+            bfs(matrix,  n);
+                
+            matrix[wires[i][0]][wires[i][1]]= 1;
+            matrix[wires[i][1]][wires[i][0]]= 1;
+        }
+        
+        return answer;
+    }
+    
+    public void bfs(int[][] matrix, int n){
+        boolean[] visited = new boolean[n+1];
+        int cnt =0;
+        Queue<Integer> q = new LinkedList<>();
+        
+        for(int i=1; i<matrix.length; i++){
+            
+            if(!visited[i]){
+                q.add(i);
+            }
+            
+            while(!q.isEmpty()){
+                int l = q.poll();
+                cnt++;
+                visited[l] = true;
+                for(int j =1; j<matrix.length; j++){
+                    if(!visited[j] && matrix[l][j]==1){
+                        q.add(j);
+                        
                     }
                 }
+               
             }
-
-            matrix[a][b] = 1;
-            matrix[b][a] = 1;
-
-            answer = Math.min(answer, Math.abs(n - nodeCount * 2));
+            answer = Math.min(Math.abs(n- cnt*2), answer);
+            
         }
-
-        return answer;
+        
+            
+        
+        
     }
 }
